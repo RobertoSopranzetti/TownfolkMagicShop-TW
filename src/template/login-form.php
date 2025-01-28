d18d6b0298b8135915d96fb49977dac743c87868
Roberto Soprnzetti <roberto.sopranzetti@studio.unibo.it>
2025-01-28 20:47:25 +0100

    made form for login

---
+++ src/template/login-form.php
@@ -0,0 +1,19 @@
+<form action="#" method="POST" class="container mt-5">
+    <h2 class="text-center">Login</h2>
+    <?php if (isset($templateParams["errorelogin"])): ?>
+        <div class="alert alert-danger" role="alert">
+            <?php echo $templateParams["errorelogin"]; ?>
+        </div>
+    <?php endif; ?>
+    <div class="mb-3">
+        <label for="identifier" class="form-label">Username o Email:</label>
+        <input type="text" class="form-control" id="identifier" name="identifier" required>
+    </div>
+    <div class="mb-3">
+        <label for="password" class="form-label">Password:</label>
+        <input type="password" class="form-control" id="password" name="password" required>
+    </div>
+    <div class="d-grid">
+        <button type="submit" class="btn btn-primary">Invia</button>
+    </div>
+</form>
