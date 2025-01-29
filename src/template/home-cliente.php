2073241b621852a3297dc72016aeb1ac94aea3b0
Roberto Soprnzetti <roberto.sopranzetti@studio.unibo.it>
2025-01-29 16:54:42 +0100

    added home for the client

---
+++ src/template/home-cliente.php
@@ -0,0 +1,16 @@
+<div class="container mt-5">
+    <div class="text-center">
+        <h1><strong>Benvenuti Avventurieri, su Townfolk Magic Shop!</strong></h1>
+        <p>Esplora il nostro negozio e scopri i prodotti magici che abbiamo da offrire.<br>
+            <i>Da ora anche con consegna gratuita al campus!</i>
+        </p>
+        <div class="d-grid gap-2 d-md-block">
+            <a href="dashboard.php" class="btn btn-info btn-lg me-md-2">
+                <i class="bi bi-search"></i> Esplora i nostri prodotti
+            </a>
+            <a href="registra-utente.php" class="btn btn-warning btn-lg">
+                <i class="bi bi-person-plus"></i> Registrati
+            </a>
+        </div>
+    </div>
+</div>
