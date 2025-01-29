8bc97aad4a93c7c1e09afaf13ebec34d861f1301
Roberto Soprnzetti <roberto.sopranzetti@studio.unibo.it>
2025-01-29 17:20:35 +0100

    added home for vendors

---
+++ src/template/home-venditore.php
@@ -0,0 +1,44 @@
+<div class="container mt-5">
+    <div class="text-center">
+        <h1><strong>Bentrovato Commerciante!</strong></h1>
+        <p>Gestisci i tuoi ordini, prodotti e altro dal tuo hub personale.</p>
+    </div>
+    <div class="row mt-4">
+        <div class="col-12 col-md-6 mb-4">
+            <div class="card text-center h-100">
+                <div class="card-body">
+                    <h5 class="card-title">Ordini in Sospeso</h5>
+                    <p class="card-text">Hai 2 ordini in sospeso da elaborare.</p>
+                    <a href="ordini.php" class="btn btn-danger">Vai agli Ordini</a>
+                </div>
+            </div>
+        </div>
+        <div class="col-12 col-md-6 mb-4">
+            <div class="card text-center h-100">
+                <div class="card-body">
+                    <h5 class="card-title">Notifiche Recenti</h5>
+                    <p class="card-text">Hai 5 nuove notifiche.</p>
+                    <a href="notifiche.php" class="btn btn-danger">Visualizza Notifiche</a>
+                </div>
+            </div>
+        </div>
+        <div class="col-12 col-md-6 mb-4">
+            <div class="card text-center h-100">
+                <div class="card-body">
+                    <h5 class="card-title">Prodotti in Esaurimento</h5>
+                    <p class="card-text">3 prodotti stanno per esaurirsi.</p>
+                    <a href="prodotti-esaurimento.php" class="btn btn-danger">Visualizza Prodotti</a>
+                </div>
+            </div>
+        </div>
+        <div class="col-12 col-md-6 mb-4">
+            <div class="card text-center h-100">
+                <div class="card-body">
+                    <h5 class="card-title">Aggiungi Prodotto</h5>
+                    <p class="card-text">Aggiungi nuovi prodotti al tuo inventario.</p>
+                    <a href="aggiungi-prodotto.php" class="btn btn-danger">Aggiungi Prodotto</a>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
