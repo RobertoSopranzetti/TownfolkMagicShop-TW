6a2206a5a2e1d011563ad411e48d11e494b414fb
Roberto Soprnzetti <roberto.sopranzetti@studio.unibo.it>
2025-01-29 18:28:43 +0100

    fixed login

---
+++ src/login.php
@@ -0,0 +1,33 @@
+<?php
+require_once 'bootstrap.php';
+
+if(isset($_POST["identifier"]) && isset($_POST["password"])){
+    $login_result = $dbh->checkLogin($_POST["identifier"], $_POST["password"]);
+    if($login_result == false){
+        //Login fallito
+        $templateParams["errorelogin"] = "Errore! Controllare username o password!";
+    }
+    else{
+        registerLoggedUser($login_result);
+        header("location: login-home.php");
+        exit();
+    }
+}
+
+if(isUserLoggedIn()){
+    $templateParams["titolo"] = "Blog TW - Admin";
+    $templateParams["nome"] = "login-home.php";
+    if(isset($_GET["formmsg"])){
+        $templateParams["formmsg"] = $_GET["formmsg"];
+    }
+}
+else{
+    $templateParams["titolo"] = "TonfolkMagicShop - Login";
+    $templateParams["nome"] = "login-form.php";
+}
+$templateParams["categorie"] = $dbh->getCategories();
+$templateParams["ruolo"] = isUserLoggedIn() ? $dbh->getRoleByUsername($_SESSION["username"])["ruolo"] : "cliente";
+
+
+require 'template/base.php';
+?>
