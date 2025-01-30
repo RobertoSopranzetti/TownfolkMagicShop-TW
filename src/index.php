bb74e69b06180fb4c7d234dea16b46d3d72ad7cc
Roberto Soprnzetti <roberto.sopranzetti@studio.unibo.it>
2025-01-30 18:31:44 +0100

    minor tweak

--- src/index.php
+++ src/index.php
@@ -4,6 +4,7 @@ require_once 'bootstrap.php';
 //Base Template
 $templateParams["titolo"] = "TownfolkMagicShop - Home";
 $templateParams["ruolo"] = isUserLoggedIn() ? $dbh->getRoleByUsername($_SESSION["username"])["ruolo"] : "cliente";
+//$templateParams["ruolo"] = "venditore";
 $templateParams["nome"] = $templateParams["ruolo"] == "venditore" ? "home-venditore.php" : "home-cliente.php";
 $templateParams["categorie"] = $dbh->getCategories();
 //Home Template
