68ec53f5bdd45282490d60edf7d695d6da4579ba
Roberto Soprnzetti <roberto.sopranzetti@studio.unibo.it>
2025-01-30 18:20:39 +0100

    added page for product insertion update and deletion

---
+++ src/gestisci-prodotti.php
@@ -0,0 +1,30 @@
+<?php
+require_once 'bootstrap.php';
+
+if(!isUserLoggedIn() || !isset($_GET["action"]) || ($_GET["action"]!=1 && $_GET["action"]!=2 && $_GET["action"]!=3) || ($_GET["action"]!=1 && !isset($_GET["id"]))){
+    header("location: login.php");
+    exit();
+}
+
+if($_GET["action"]!=1){
+    $risultato = $dbh->getProductByIdAndSeller($_GET["id"], $_SESSION["idutente"]);
+    if(count($risultato)==0){
+        $templateParams["prodotto"] = null;
+    }
+    else{
+        $templateParams["prodotto"] = $risultato[0];
+    }
+}
+else{
+    $templateParams["prodotto"] = getEmptyProduct();
+}
+
+$templateParams["titolo"] = "TownfolkMagicShop - Gestisci Prodotti";
+$templateParams["nome"] = "product-form.php";
+$templateParams["categorie"] = $dbh->getCategories();
+$templateParams["prodottiCasuali"] = $dbh->getRandomProducts(2);
+
+$templateParams["azione"] = $_GET["action"];
+
+require 'template/base.php';
+?>
