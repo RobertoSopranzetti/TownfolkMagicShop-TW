4eb4752149fb8f83391dbcde92e71f161ec01a68
Roberto Soprnzetti <roberto.sopranzetti@studio.unibo.it>
2025-01-31 18:59:36 +0100

    added dashboard

---
+++ src/dashboard.php
@@ -0,0 +1,33 @@
+<?php
+require_once 'bootstrap.php';
+require_once 'utils/functions.php';
+
+$templateParams["titolo"] = "TownfolkMagicShop - Prodotti";
+$templateParams["ruolo"] = isUserLoggedIn() ? $dbh->getRoleByUsername($_SESSION["username"])["ruolo"] : "cliente";
+
+$minPrice = $dbh->getMinPrice();
+$maxPrice = $dbh->getMaxPrice();
+$selectedCategory = isset($_GET['categoria']) ? $_GET['categoria'] : '';
+$selectedMaxPrice = isset($_GET['prezzoMax']) ? $_GET['prezzoMax'] : $maxPrice;
+$limitedEdition = isset($_GET['edizioneLimitata']) && $_GET['edizioneLimitata'] == '1' ? true : false;
+
+$templateParams["categorie"] = $dbh->getCategories();
+$templateParams["prezzoMin"] = $minPrice;
+$templateParams["prezzoMax"] = $maxPrice;
+$templateParams["selectedCategory"] = $selectedCategory;
+$templateParams["selectedMaxPrice"] = $selectedMaxPrice;
+$templateParams["limitedEdition"] = $limitedEdition;
+
+// Logica per filtrare i prodotti
+if ($templateParams["ruolo"] == "venditore") {
+    $prodotti = $dbh->getProductsByUserId($_SESSION["idutente"]);
+} else {
+    $prodotti = $dbh->getProductsByPriceRange($minPrice, $selectedMaxPrice);
+    $prodotti = filterProducts($prodotti, $selectedCategory, $limitedEdition);
+}
+
+$templateParams["prodotti"] = $prodotti;
+$templateParams["nome"] = "lista-prodotti.php";
+
+require 'template/base.php';
+?>
