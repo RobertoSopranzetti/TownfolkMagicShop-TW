fa6446fb7297a01a61b64b836dd14904d90cc273
Roberto Soprnzetti <roberto.sopranzetti@studio.unibo.it>
2025-02-04 13:44:09 +0100

    formatting

--- src/login.php
+++ src/login.php
@@ -1,33 +1,29 @@
 <?php
 require_once 'bootstrap.php';
 
-if(isset($_POST["identifier"]) && isset($_POST["password"])){
+if (isset($_POST["identifier"]) && isset($_POST["password"])) {
     $login_result = $dbh->checkLogin($_POST["identifier"], $_POST["password"]);
-    if($login_result == false){
+    if ($login_result == false) {
         //Login fallito
         $templateParams["errorelogin"] = "Errore! Controllare username o password!";
-    }
-    else{
+    } else {
         registerLoggedUser($login_result);
         header("location: login-home.php");
         exit();
     }
 }
 
-if(isUserLoggedIn()){
+if (isUserLoggedIn()) {
     $templateParams["titolo"] = "TownfolkMagicShop - User Home";
     $templateParams["nome"] = "login-home.php";
-    if(isset($_GET["formmsg"])){
+    if (isset($_GET["formmsg"])) {
         $templateParams["formmsg"] = $_GET["formmsg"];
     }
-}
-else{
+} else {
     $templateParams["titolo"] = "TownfolkMagicShop - Login";
     $templateParams["nome"] = "login-form.php";
 }
 $templateParams["prodottiCasuali"] = $dbh->getRandomProducts(2);
 $templateParams["ruolo"] = isUserLoggedIn() ? $dbh->getRoleByUsername($_SESSION["username"])["ruolo"] : "cliente";
-
-
 require 'template/base.php';
 ?>
