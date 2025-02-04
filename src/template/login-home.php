a0a139a8c094a3dc6d1acdae44115cb3886f9c04
Roberto Soprnzetti <roberto.sopranzetti@studio.unibo.it>
2025-02-04 16:32:26 +0100

    more accessible tweak

--- src/template/login-home.php
+++ src/template/login-home.php
@@ -5,14 +5,14 @@
             <a href="index.php" class="btn btn-primary btn-lg me-md-2 mb-2" aria-label="Torna alla Home">
                 <i class="bi bi-house"></i> Torna alla Home
             </a>
-            <a href="ordini.php" class="btn btn-primary btn-lg me-md-2 mb-2" aria-label="Vedi Ordini">
+            <a href="ordini.php" class="btn btn-success btn-lg me-md-2 mb-2" aria-label="Vedi Ordini">
                 <i class="bi bi-card-list"></i> Vedi Ordini
             </a>
-            <a href="notifiche.php" class="btn btn-primary btn-lg me-md-2 mb-2" aria-label="Vedi Notifiche">
+            <a href="notifiche.php" class="btn btn-success btn-lg me-md-2 mb-2" aria-label="Vedi Notifiche">
                 <i class="bi bi-bell"></i> Vedi Notifiche
             </a>
             <form action="logout.php" method="POST" class="d-inline">
-                <button type="submit" class="btn btn-primary btn-lg me-md-2 mb-2" aria-label="Logout">
+                <button type="submit" class="btn btn-danger btn-lg me-md-2 mb-2" aria-label="Logout">
                     <i class="bi bi-box-arrow-right"></i> Logout
                 </button>
             </form>
