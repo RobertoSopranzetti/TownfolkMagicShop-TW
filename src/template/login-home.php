93537441d1d4d460494dacb3a2e659d71a161260
Roberto Soprnzetti <roberto.sopranzetti@studio.unibo.it>
2025-02-04 16:26:37 +0100

    made user home accessible

--- src/template/login-home.php
+++ src/template/login-home.php
@@ -1,18 +1,18 @@
 <section class="container mt-5">
     <div class="text-center">
-        <h1>Ciao <?php echo $_SESSION["username"]; ?>! Come posso aiutarti?</h1>
+        <h1 class="mb-4">Ciao <?php echo $_SESSION["username"]; ?>! Come posso aiutarti?</h1>
         <div class="d-grid gap-2 d-md-block mt-4">
-            <a href="index.php" class="btn btn-primary btn-lg me-md-2">
+            <a href="index.php" class="btn btn-primary btn-lg me-md-2 mb-2" aria-label="Torna alla Home">
                 <i class="bi bi-house"></i> Torna alla Home
             </a>
-            <a href="ordini.php" class="btn btn-primary btn-lg me-md-2">
+            <a href="ordini.php" class="btn btn-primary btn-lg me-md-2 mb-2" aria-label="Vedi Ordini">
                 <i class="bi bi-card-list"></i> Vedi Ordini
             </a>
-            <a href="notifiche.php" class="btn btn-primary btn-lg me-md-2">
+            <a href="notifiche.php" class="btn btn-primary btn-lg me-md-2 mb-2" aria-label="Vedi Notifiche">
                 <i class="bi bi-bell"></i> Vedi Notifiche
             </a>
             <form action="logout.php" method="POST" class="d-inline">
-                <button type="submit" class="btn btn-primary btn-lg me-md-2">
+                <button type="submit" class="btn btn-primary btn-lg me-md-2 mb-2" aria-label="Logout">
                     <i class="bi bi-box-arrow-right"></i> Logout
                 </button>
             </form>
